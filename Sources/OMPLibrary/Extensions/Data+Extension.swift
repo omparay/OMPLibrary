d00c02a8f6c178dda4cc27c783f67d2d15891a91
Oliver Paray <omparay@gmail.com>
2020-04-05 11:01:36 -0700

    Mockup implementations for Hasher and the Data extension

---
+++ Sources/OMPLibrary/Extensions/Data+Extension.swift
@@ -0,0 +1,26 @@
+//
+// Data+Extension.swift
+//
+// Created by Oliver Paray on 04/05/2020
+//
+
+import Foundation
+
+public extension Data{
+
+    //MARK: Properties
+    
+    
+    //MARK: Initializers
+    
+    
+    //MARK: Class Methods
+    
+    
+    //MARK: Public Methods
+    
+    
+    //MARK: Private Methods
+    
+    
+}
---
+++ Sources/OMPLibrary/Utilities/Hasher.swift
@@ -0,0 +1,35 @@
+//
+// Hasher.swift
+//
+// Created by Oliver Paray on 04/05/2020
+//
+
+import Foundation
+
+public class Hasher{
+
+    //MARK: Properties
+    
+    
+    //MARK: Initializers
+    
+    
+    //MARK: Class Methods
+    
+    
+    //MARK: Public Methods
+    
+    
+    //MARK: Private Methods
+    
+    
+    //MARK: Delegates
+    
+    
+    //MARK: Datasource
+    
+    
+    //MARK: Handlers
+    
+    
+}
